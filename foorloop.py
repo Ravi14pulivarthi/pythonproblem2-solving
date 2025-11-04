f80aa449d27c0ea0d570f5507fafcda6c6636145
Ravikumar <118242322+Ravi14pulivarthi@users.noreply.github.com>
2025-11-04 10:22:55 +0530

    python pratic

---
+++ foorloop.py
@@ -0,0 +1,45 @@
+# names=["Ravi","Kumar","Pulivarthi"]
+# for i in names:
+#     print(i)
+    
+
+# for i in range(5):
+    
+#     print(i+1)
+    
+# for i in range(1,6):
+#     print(i)
+    
+    
+# Problem1
+#  //Print numbers from 1 to 10.
+
+# for i in range(1,11):
+#     print(i)
+
+# Print even numbers between 1 and 20.
+
+# for i in range(1,21):
+#     if(i %2 ==0):
+#         print(i)
+    
+     
+# Print the sum of numbers from 1 to 10.
+
+# sum=0
+# for i in range(1,11):
+#     sum+=i
+# print(sum)
+
+# Print the 5th table 
+
+# num=5
+
+# for i in range(1,6):
+#     print(num,"*",i,"=",num*i)
+
+name="Python"
+
+for i in range(len(name)):
+    print(i,"index of ecah char of string is :",name[i] )
+    
